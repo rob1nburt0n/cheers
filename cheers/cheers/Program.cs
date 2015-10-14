ea63d3b20d9234b0f0bc2017289de8be1206dca5
rob1nburt0n <1.robinburton@gmail.com>
2015-10-14 09:04:58 -0700

    first commit

---
+++ cheers/cheers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cheers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello there! What's your name?");
+            string name = System.Console.ReadLine();
+            System.Console.WriteLine("Hi, " + name);
+
+            foreach (char letter in name)
+            {
+               string article = "a";
+                 foreach (char vowelSound in "halfnorsemixHALFNORSMIX")
+                 {
+                 if (letter == vowelSound)
+                       {
+                          article = "an";
+                        }
+                        }
+               Console.WriteLine("Give me " + article + " " + letter);
+                          }
+               
+                Console.WriteLine(name.ToUpper() + " is GRAND");
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+        }
+    }
+}
