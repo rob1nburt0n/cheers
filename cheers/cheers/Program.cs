a2aab98eece5b048a3144520c4848128dc87c7e2
rob1nburt0n <1.robinburton@gmail.com>
2015-10-15 14:08:00 -0500

    Start on Cheers part 2

--- cheers/cheers/Program.cs
+++ cheers/cheers/Program.cs
@@ -13,21 +13,37 @@ namespace cheers
             System.Console.WriteLine("Hello there! What's your name?");
             string name = System.Console.ReadLine();
             System.Console.WriteLine("Hi, " + name);
+            System.Console.WriteLine("What's your birthday? (MM/DD)");
+            string bday = System.Console.ReadLine();
+            DateTime convertedBday = Convert.ToDateTime(bday);
+            DateTime today = DateTime.Today;
 
             foreach (char letter in name.ToLower())
             {
-               string article = "a";
-                 foreach (char vowelSound in "halfnorsemix")
-                 {
-                 if (letter == vowelSound)
-                       {
-                          article = "an";
+                    string article = "a";
+                    foreach (char vowelSound in "halfnorsemix")
+                    {
+                        if (letter == vowelSound)
+                        {
+                            article = "an";
                         }
-                        }
+                    }
                Console.WriteLine("Give me " + article + " " + letter);
                           }
                
                 Console.WriteLine(name.ToUpper() + " is GRAND");
+            if (convertedBday.Equals(today))
+            {
+                Console.WriteLine("Happy Birthday!!");
+            }
+            else
+            {
+                if (convertedBday < today)
+                {
+                    convertedBday = convertedBday.AddYears(1);
+                }
+                Console.WriteLine("Your birthday is " + convertedBday.Subtract(today).Days + " days away! Happy Birthday in Advance!");
+            }
                 Console.WriteLine("Press any key to exit.");
                 Console.ReadKey();
         }
